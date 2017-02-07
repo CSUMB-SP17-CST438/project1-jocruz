70ac1229bb39fb1911ce64253704296c903db3cc
jcrzry <jcrzry@yahoo.com>
2017-02-07 06:22:43 +0000

    fixed port

--- flasky.py
+++ flasky.py
@@ -21,7 +21,7 @@ def main():
     
     
 app.run(
-    # port = int(os.getenv('PORT',8080)),
-    # host = os.getenv('IP','0.0.0.0'),
+    port = int(os.getenv('PORT')),
+    host = os.getenv('IP','0.0.0.0'),
     # debug=True
 )
